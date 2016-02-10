de636ed6f530e14469e54b8c0aab4c4a61e1cb49
Tushar Nakini <tnb88@mail.missouri.edu>
2016-02-10 09:46:17 -0600

    Updated comments only

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The inverse of a invertible square matrix is evaluated using the following functions. First, the matrix is store in a list-object using the function "makeCacheMatrix". Then the inverse is calculated using the function "cacheSolve". The first call to the cacheSolve function evaluates the inverse, then stores the result in the same matrix object cache, and returns the result, in the end. The subsequent calls to the function "cacheSolve" for the same object will only return the inverse which is stored in the matrix-object cache.
 
 ## makeCacheMatrix --
 ## In this method a special object is created which will hold the data and the mean values of the data. It has couple of get/set methods to retrieve/store the data and it's mean value.
@@ -30,6 +29,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## cacheSolve
 # This method takes a special matrix object created using "makeCacheMatrix" function and evaluates the inverse of the matrix and then stores the inverse in the same object. The stored inverse matrix acts like a cache if the object is not destroyed. So, when cacheSolve function is called for a nxn matrix for the first time, the computational complexity will be O(n^3). However, the computation complexity of subsequent function calls for the same matrix object is O(1), because the value is accessed from the cache.
+# INPUT:
+#   x -- Invertible square matrix object.
+# OUTPUT:
+#   Inverse matrix.
 cacheSolve <- function(x, ...) {
     # Get the cache value and check whether it is NULL value or not. If it's not a NULL, then return the inverse of the matrix and exit.
     invMatrix <- x$getinverse()
